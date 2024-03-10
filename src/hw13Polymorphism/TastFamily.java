5d7040c3063d9132825fc07b424ec4c454d99a07
Jubraz007 <jubraz00@gmail.com>
2024-03-10 00:39:21 -0500

    updated

--- src/hw13Polymorphism/TastFamily.java
+++ src/hw13Polymorphism/TastFamily.java
@@ -10,18 +10,21 @@ public class TastFamily {
 		sister.sister(10, 15);
 		sister.sister(20, 25, 24);
 		sister.sister(28, "35", 23);
-		sister.sister("50", 45,34);
+		sister.sister("50", 45, 34);
 		sister.sister(28, 30, 35, 40);
-		Sister.sister(20, 31, 37, 43, 49);
+		Sister.sister(20, 31, 37, 43, 49);// Static method has to be called with direct class name
 
 		System.out.println("\n---- Niece----");
-		Niece niece = new Niece()	;
+		// object of sister class
+		Niece niece = new Niece();
 		niece.sister();// void type method 01
 		niece.sister(10, 15);
 		niece.sister(20, 25, 24);
 		niece.sister(28, "35", 23);
-		niece.sister("50", 45,34);
-	
+		niece.sister("50", 45, 34);
+		// final and static method cannot be called because it was not override in child
+		// class
+		// final and static method can't be override
 	}
 
 }
